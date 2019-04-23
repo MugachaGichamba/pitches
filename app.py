476185de29bf2b734618bd49909324d1e628d301
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-23 09:59:26 +0300

    populated pitch with dummy data in a list

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+pitches = [
+    {
+        'author': 'Mugacha Gichamba',
+        'category': "wordplay",
+        'pitch': "mkinunua loaf haiko sliced lazima mkate",
+        "likes": 3,
+        "dislikes": 0,
+        "comments": ["fiti", "kijana round"],
+        "date posted": "April 23rd 2019"
+
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
