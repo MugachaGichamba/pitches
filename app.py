a62ec3a4a02f8e4b5a3ac4f66b1e1db698ee0378
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-23 12:57:43 +0300

    implemented url_for for every route

--- app.py
+++ app.py
@@ -45,9 +45,17 @@ def register():
     return render_template('register.html', title="Register", form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@gmail.com" and form.password.data == "pass":
+            flash("you have been logged in", 'success')
+            return redirect(url_for('home'))
+
+        else:
+            flash("invalid credentials", 'danger')
+
     return render_template('login.html', title="Login", form=form)
 
 
