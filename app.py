bc0f42b7f9df09b3f66f4f532210e18fe4ea3d76
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-23 11:43:03 +0300

    login and registration routes done

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
@@ -34,9 +36,16 @@ def home():
     return render_template('home.html', pitches=pitches, title="Home")
 
 
-@app.route('/about')
-def about():
-    return 'about!'
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
+
 
 
 if __name__ == '__main__':
