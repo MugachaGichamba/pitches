7c95bda785cdedbe288527caa47c0eb1cfa6ed5d
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-24 11:08:05 +0300

    imported and initialised bcrypt

--- piches/__init__.py
+++ piches/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f04d4ad95aa5cb663854acb929d69a01'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from piches import routes
