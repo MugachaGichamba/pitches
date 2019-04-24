839ec4d8a0a772de5c40c1ed4a4f0253913f1444
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-24 09:48:45 +0300

    moved models from main to separate file

--- app.py
+++ app.py
@@ -1,38 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from models import User,Pitch
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f04d4ad95aa5cb663854acb929d69a01'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.png')
-    password = db.Column(db.String(60), nullable=False)
-    pitches = db.relationship('Pitch', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}, {self.email}, {self.image_file}')"
-
-
-class Pitch(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(50), nullable=False)
-    category = db.Column(db.String(200), nullable=False)
-    pitch = db.Column(db.String(200), nullable=False)
-    likes = db.Column(db.Integer, default=0)
-    dislikes = db.Column(db.Integer, default=0)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-    def __repr__(self):
-        return f"Pitch('{self.pitch}, {self.date_posted}')"
 
 
 pitches = [
